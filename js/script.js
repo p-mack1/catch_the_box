84c3d4b599541ad3704de54197133b9dc1864517
p-mack1 <mackphillip3@gmail.com>
2026-08-19 10:49:21 -0400

    Formatting

--- js/script.js
+++ js/script.js
@@ -1,64 +1,55 @@
-const target= document.getElementById("target");
-const startButton= document.getElementById("startButton");
-const scoreDisplay= document.getElementById("score");
-const timeDisplay= document.getElementById("time");
-const messageDisplay= document.getElementById("message");
+const target = document.getElementById('target');
+const startButton = document.getElementById('startButton');
+const scoreDisplay = document.getElementById('score');
+const timeDisplay = document.getElementById('time');
+const messageDisplay = document.getElementById('message');
 
 let score = 0;
 let time = 15;
 let gamerunning = false;
 
 function moveTarget(X, y) {
+  const maxX = 540;
+  const maxY = 340;
 
-    const maxX =540;
-    const maxY= 340;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    target.style.left = randomX+ "px";
-    target.style.top = randomY + "px";
-
+  target.style.left = randomX + 'px';
+  target.style.top = randomY + 'px';
 }
 
-target.addEventListener("click", function () {
-    
-   if(gamerunning === true) { 
+target.addEventListener('click', function () {
+  if (gamerunning === true) {
     score = score + 2;
 
- scoreDisplay.textContent = score;
+    scoreDisplay.textContent = score;
 
-   moveTarget();
-   
-}
+    moveTarget();
+  }
+});
 
-})
+startButton.addEventListener('click', function () {
+  score = 0;
+  timeLeft = 15;
+  gamerunning = true;
+  scoreDisplay.textContent = score;
+  timeDisplay.textContent = timeLeft;
 
+  messageDisplay.textContent = '';
+  target.style.display = 'block';
 
-  startButton.addEventListener("click", function () {
-    
-   score = 0;
-   timeLeft = 15;
-   gamerunning = true;
-   scoreDisplay.textContent = score;
-   timeDisplay.textContent = timeLeft;
-
-   messageDisplay.textContent = "";
-   target.style.display = "block";
-
-   const timer = setInterval(function () {
+  const timer = setInterval(function () {
     timeLeft = timeLeft - 1;
     timeDisplay.textContent = timeLeft;
 
     if (timeLeft === 0) {
-     clearInterval(timer);
-     gamerunning = false;
-     target.style.display = "none";
-     messageDisplay.textContent = "Game Over! Your score is: " + score;
+      clearInterval(timer);
+      gamerunning = false;
+      target.style.display = 'none';
+      messageDisplay.textContent = 'Game Over! Your score is: ' + score;
     }
+  }, 1000);
 
-   },1000);
-  
-    
-    moveTarget();
+  moveTarget();
 });
